23b26b993e6f94ab20c5411e71de30c5bc049ac1
Maximilian Falk <maximilian.falk@zdf.digital>
2024-07-12 13:24:44 +0200

    added cli enum args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-use std::env;
+#![allow(dead_code)]
+#![allow(unreachable_code)]
 
-use anyhow::{Result};
-use opencv::{self as cv, core::{flip, transpose, Rect, Scalar, CV_8UC3, Vec3b}, highgui, imgproc, prelude::*, videoio};
+use std::{default, env};
+
+use anyhow::Result;
+use clap::Parser;
+use clap::{arg, Command, ValueEnum};
+use opencv::{
+    self as cv,
+    core::{flip, transpose, Rect, Scalar, Vec3b, CV_8UC3},
+    highgui, imgproc,
+    prelude::*,
+    videoio,
+};
 use smart_leds::RGB8;
 
 fn mat_to_rgb8_array(mat: &Mat) -> Result<Vec<RGB8>> {
@@ -24,24 +35,61 @@ fn mat_to_rgb8_array(mat: &Mat) -> Result<Vec<RGB8>> {
             })
         }
     }
-
     Ok(rgb8_array)
+}
+
+#[derive(Parser)]
+#[command(name = "rustylight")]
+#[command(
+    about = "Diy ambilight implementation",
+    long_about = "Takes video data and maps the edges onto an LED strip"
+)]
+struct Cli {
+    #[arg(short, long, value_enum, required = true)]
+    start_corner: StartCorner,
+
+    #[arg(short, long, value_enum, required = true)]
+    direction: Direction,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Default)]
+enum StartCorner {
+    /// Top Left Corner
+    #[default]
+    TL,
+    /// Top Right Corner
+    TR,
+    /// Bottom Left Corner
+    BL,
+    /// Bottom Right Corner
+    BR,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Default)]
+enum Direction {
+    /// Clockwise
+    #[default]
+    CW,
+    /// Counter Clockwise
+    CCW,
 }
 
 fn main() -> Result<()> {
     dotenvy::dotenv()?;
 
-    let matches = Command::new("MyApp")
-        .version("1.0")
-        .about("Does awesome things")
-        .arg(arg!(--two <VALUE>).required(true))
-        .arg(arg!(--one <VALUE>).required(true))
-        .get_matches();
+    let cli = Cli::parse();
 
-    println!(
-        "two: {:?}",
-        matches.get_one::<String>("two").expect("required")
-    );
+    match cli.start_corner {
+        StartCorner::TR => println!("Top Right selected"),
+        StartCorner::TL => println!("Top Left selected"),
+        StartCorner::BR => println!("Bottom Right selected"),
+        StartCorner::BL => println!("Bottom Left selected"),
+    }
+
+    match cli.direction {
+        Direction::CW => println!("Clockwise selected"),
+        Direction::CCW => println!("Counter Clockwise selected"),
+    }
 
     #[cfg(feature = "highgui")]
     {
@@ -49,8 +97,9 @@ fn main() -> Result<()> {
         highgui::named_window("frame", highgui::WINDOW_NORMAL)?;
     }
 
-    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
-    //let mut cam = videoio::VideoCapture::from_file("/home/max/Downloads/test_vid.mp4", videoio::CAP_ANY)?;
+    //let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
+    let mut cam =
+        videoio::VideoCapture::from_file("/home/max/Downloads/test_vid.mp4", videoio::CAP_ANY)?;
     let mut orig_frame = Mat::default();
 
     // Get the size of the video feed
@@ -60,16 +109,36 @@ fn main() -> Result<()> {
     let border_thickness = env::var("BORDER_THICKNESS")?.parse()?;
 
     let horizontal_main = orig_frame.cols();
-    let vertical_main = orig_frame.rows() - (2* border_thickness);
+    let vertical_main = orig_frame.rows() - (2 * border_thickness);
 
-    let mut border_frame = Mat::new_rows_cols_with_default(border_thickness, (2*orig_frame.cols())+(2*(orig_frame.rows() - 2*border_thickness)), orig_frame.typ(), Scalar::all(0.0))?;
+    let mut border_frame = Mat::new_rows_cols_with_default(
+        border_thickness,
+        (2 * orig_frame.cols()) + (2 * (orig_frame.rows() - 2 * border_thickness)),
+        orig_frame.typ(),
+        Scalar::all(0.0),
+    )?;
 
-    // Borders of the video 
+    // Borders of the video
     let borders = [
         Rect::new(0, 0, size.width, border_thickness),
-        Rect::new(0, size.height - border_thickness, size.width, border_thickness),
-        Rect::new(0, border_thickness, border_thickness, size.height-(2*border_thickness)),
-        Rect::new(size.width - border_thickness, border_thickness, border_thickness, size.height - (2*border_thickness)),
+        Rect::new(
+            0,
+            size.height - border_thickness,
+            size.width,
+            border_thickness,
+        ),
+        Rect::new(
+            0,
+            border_thickness,
+            border_thickness,
+            size.height - (2 * border_thickness),
+        ),
+        Rect::new(
+            size.width - border_thickness,
+            border_thickness,
+            border_thickness,
+            size.height - (2 * border_thickness),
+        ),
     ];
 
     loop {
@@ -79,7 +148,6 @@ fn main() -> Result<()> {
         let src_roi = Mat::roi(&orig_frame, borders[0])?;
         let mut dst_roi = Mat::roi_mut(&mut border_frame, borders[0])?;
         src_roi.copy_to(&mut dst_roi)?;
-
 
         // Transpose and flip right border
         let src_roi = Mat::roi(&orig_frame, borders[3])?;
@@ -93,17 +161,20 @@ fn main() -> Result<()> {
         let mut dst_roi = Mat::roi_mut(&mut border_frame, right_border_target)?;
         right_flipped_roi.copy_to(&mut dst_roi)?;
 
-
-        // Flip bottom roi 
+        // Flip bottom roi
         let src_roi = Mat::roi(&orig_frame, borders[1])?;
         let mut bottom_flipped_roi = Mat::default();
         flip(&src_roi, &mut bottom_flipped_roi, -1)?;
 
         // Copy flipped
-        let bottom_border_target = Rect::new(horizontal_main + vertical_main, 0, horizontal_main, border_thickness);
+        let bottom_border_target = Rect::new(
+            horizontal_main + vertical_main,
+            0,
+            horizontal_main,
+            border_thickness,
+        );
         let mut dst_roi = Mat::roi_mut(&mut border_frame, bottom_border_target)?;
         bottom_flipped_roi.copy_to(&mut dst_roi)?;
-
 
         // Transpose left border
         let src_roi = Mat::roi(&orig_frame, borders[2])?;
@@ -113,10 +184,38 @@ fn main() -> Result<()> {
         flip(&left_transposed_roi, &mut left_flipped, 1)?;
 
         // Copy to destination
-        let left_border_target = Rect::new(2*horizontal_main + vertical_main, 0, vertical_main, border_thickness);
+        let left_border_target = Rect::new(
+            2 * horizontal_main + vertical_main,
+            0,
+            vertical_main,
+            border_thickness,
+        );
         let mut dst_roi = Mat::roi_mut(&mut border_frame, left_border_target)?;
         left_flipped.copy_to(&mut dst_roi)?;
 
+        let mut mean_colors = Vec::<RGB8>::with_capacity(border_frame.cols() as usize);
+
+        for col in 0..border_frame.cols() {
+            let mut sum_r = 0;
+            let mut sum_g = 0;
+            let mut sum_b = 0;
+
+            for row in 0..border_frame.rows() {
+                let pixel = border_frame.at_2d::<Vec3b>(row, col)?;
+
+                sum_b += pixel[0] as u32;
+                sum_g += pixel[1] as u32;
+                sum_r += pixel[2] as u32;
+            }
+
+            sum_r /= border_frame.rows() as u32;
+            sum_g /= border_frame.rows() as u32;
+            sum_b /= border_frame.rows() as u32;
+
+            mean_colors.push(RGB8::new(sum_r as u8, sum_g as u8, sum_b as u8));
+        }
+
+        //println!("{:?}", mean_colors);
         // To RGB8 for driving LEDs
         //println!("{:?}", mat_to_rgb8_array(&border_frame));
 
@@ -126,7 +225,8 @@ fn main() -> Result<()> {
             highgui::imshow("frame", &border_frame)?;
 
             let key = highgui::wait_key(1)?;
-            if key == 113 { // quit with q
+            if key == 113 {
+                // quit with q
                 break;
             }
         }
